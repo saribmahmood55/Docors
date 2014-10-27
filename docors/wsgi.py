3e51e02d27772b95093c6788ddffc1fc3b19128d
asdi744 <anrana744@gmail.com>
2014-10-27 18:53:36 +0500

    db data from fixtures

--- docors/wsgi.py
+++ docors/wsgi.py
@@ -1,19 +1,8 @@
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "docors.settings")
-
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
-application = Cling(get_wsgi_application())
-
-
-'''
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "docors.settings")
 
-from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
-
-application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
-'''
+application = Cling(get_wsgi_application())
+#application = get_wsgi_application()
