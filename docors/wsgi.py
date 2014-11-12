a620a2b6290848d6865e3fe3795319904dbb2549
asdi744 <anrana744@gmail.com>
2014-11-13 02:08:47 +0500

    update wsgi file

--- docors/wsgi.py
+++ docors/wsgi.py
@@ -2,6 +2,14 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "docors.settings")
 
 from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
+
+'''
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "docors.settings")
+
+from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
 application = Cling(get_wsgi_application())
+'''
