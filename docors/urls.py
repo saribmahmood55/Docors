6278c2db7b6ca982ec05694fd72c21aa1d6c2629
asdi744 <anrana744@gmail.com>
2015-01-14 00:59:03 +0500

    static url fix

--- docors/urls.py
+++ docors/urls.py
@@ -11,11 +11,16 @@ urlpatterns = patterns('',
     url(r'^', include('patients.urls')),
     url(r'^', include('reviews.urls')),
     url(r'^', include('registration.backends.simple.urls')),
-    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', { 'document_root': settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
     url(r'^asad-rana/', include(admin.site.urls)),
     url(r'^password/reset/$', 'django.contrib.auth.views.password_reset',{'post_reset_redirect' : '/password/reset/done/'}),
     url(r'^password/reset/done/$', 'django.contrib.auth.views.password_reset_done'),
     url(r'^password/reset/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', 'django.contrib.auth.views.password_reset_confirm', {'post_reset_redirect' : '/password/done/'}),
     url(r'^password/done/$', 'django.contrib.auth.views.password_reset_complete'),
 )
+if not settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^static/media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT, 'show_indexes':True}),
+        )
+    urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT, 'show_indexes':True}),
+        )
