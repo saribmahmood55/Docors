667d201197c6e629d2670159b21869bd6a8ee326
asdi744 <anrana744@gmail.com>
2015-01-29 18:58:57 +0500

    celery renamed to celery_instance

--- practitioner/__init__.py
+++ practitioner/__init__.py
@@ -1,2 +1,2 @@
 from __future__ import absolute_import
-from docors.celery import app as celery_app
+from docors.celery_instance import app as celery_app
--- practitioner/tasks.py
+++ practitioner/tasks.py
@@ -12,7 +12,7 @@ def add(x, y):
 @shared_task
 def confirmation_mail(email_details):
 	subject = "Registration Request | " + email_details['name']
-	message = "Hi "+email_details['name']+",\n\nYour Practice information has been recieved successfully, It will be availabe to everyone on http://www.doctorsinfo.pk in next 24-36 Hours.\nDoctors Info. Pakistan will never public your email address.\nIn order to make any changes in your Practice details(i.e Contact Number, Address, Check up fee or Timings) you can drop us an email with that email id and subject as: "+email_details['slug']+"\n\nKeep Serving Pakistan !\n\nRegards:\nDoctors Info. Pakistan."
+	message = "Hi "+email_details['name']+",\n\nYour Practice information has been recieved successfully, It will be availabe to everyone on http://www.doctorsinfo.pk in next 24-36 Hours. Doctors Info. Pakistan will never public your email address.\nIn order to make any changes in your Practice details(i.e Contact Number, Address, Check up fee or Timings) you can drop us an email with that email id and subject as: "+email_details['slug']+"\n\nKeep Serving Pakistan !\n\nRegards:\nDoctors Info. Pakistan."
 	sender = 'doctorsinfo.pk@gmail.com'
 	recepient = email_details['email']
 	if send_mail(subject, message, sender, [recepient], fail_silently=True):
