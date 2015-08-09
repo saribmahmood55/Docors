3c387b3ffc50d7bc818075822b63a5504bc0267a
Sarib Mahmood <saribmahmood55@gmail.com>
2015-08-10 01:08:17 +0500

    patients admin file

--- patients/admin.py
+++ patients/admin.py
@@ -1,6 +1,5 @@
 # flake8: noqa
 from django.contrib import admin
-from practitioner.models import Practitioner, Specialization
 from patients.models import *
 
 class SubscriptionAdmin(admin.ModelAdmin):
@@ -14,10 +13,10 @@ class PatientAdmin(admin.ModelAdmin):
 
 	def patient_user_name(self, obj):
 		return obj.email
-	
+
 	def patient_name(self, obj):
 		return "%s" % (obj.full_name)
-	
+
 	def email(self, obj):
 		return obj.email
 
@@ -25,7 +24,7 @@ class PatientAdmin(admin.ModelAdmin):
 		return "\n".join([specialities.name for specialities in obj.interested_specialities.all()])
 
 	def Favourite_Practitioners(self, obj):
-		return "\n".join([practitioner.name for practitioner in obj.favt_practitioner.all()])
+		return "\n".join([practitioner.full_name for practitioner in obj.favt_practitioner.all()])
 
 admin.site.register(Subscription, SubscriptionAdmin)
 admin.site.register(Patient, PatientAdmin)
