30a884b506db1c0bb6f1a7c11a0aec103308918e
Asad Naeem <anrana744@gmail.com>
2015-08-18 00:55:01 +0500

    updated: page specific fb url tags

--- docors/forms.py
+++ docors/forms.py
@@ -10,7 +10,7 @@ class doctors_form(forms.Form):
 
 class speciality_form(forms.Form):
 
-	spec = forms.ChoiceField(choices = [(r.id, r.human_name) for r in Specialization.objects.order_by('name')], label='Speciality.', required = True)
+	spec = forms.ChoiceField(choices = [(r.id, r.human_name) for r in Specialization.objects.order_by('name')], label='Specialty.', required = True)
 	area = forms.ChoiceField(choices = [(r.id, r.name) for r in Area.objects.filter(city=City.objects.get(name='Lahore'))], label='Area.', required = True)
 
 class advanced_form(forms.Form):
@@ -18,7 +18,7 @@ class advanced_form(forms.Form):
 	RADIUS_CHOICES = ((1, '1'), (2, '2'), (5, '5'), (10, '10'), (15, '15'), (20, '20'),)
 	DAYS = (('1', 'Monday'), ('2', 'Tuesday'), ('3', 'Wednesday'), ('4', 'Thursday'), ('5', 'Friday'),('6', 'Saturday'),('7', 'Sunday'),)
 
-	spec_adv = forms.ChoiceField(choices = [(r.id, r.human_name) for r in Specialization.objects.order_by('name')], label='Speciality.', required = True)
+	spec_adv = forms.ChoiceField(choices = [(r.id, r.human_name) for r in Specialization.objects.order_by('name')], label='Specialty.', required = True)
 	lon = forms.CharField(label='Physical longitude.', widget=forms.HiddenInput(), required=False)
 	lat = forms.CharField(label='Physical latitude.', widget=forms.HiddenInput(), required=False)
 	radius = forms.ChoiceField(choices = RADIUS_CHOICES, label="Radius (KM)", initial='2', required = True)
--- practitioner/urls.py
+++ practitioner/urls.py
@@ -1,19 +1,20 @@
+#flake8: noqa
 from django.conf.urls import patterns, url
 from practitioner import views
 
 urlpatterns = patterns('',
-                       #Search Related urls
-                       url(r'^physican/suggestions/$', views.suggestions_practitioner, name='physican_suggestions'),
-                       url(r'^physican/search/$', views.search_practitioner, name='physican_search'),
-                       url(r'^(?P<slug>[a-z-]+)-in-(?P<typee>[A-Za-z-]+)$', views.results_practitioner, name='physican_results'),
-                       #Physican Page urls
-                       url(r'^physican/(?P<slug>[\w-]+)/claim/$', views.claim_practitioner, name='claim_practitioner'),
-                       url(r'^physican/(?P<slug>[\w-]+)/update/info/$', views.update_info_practitioner, name='update_info_practitioner'),
-                       url(r'^physican/(?P<slug>[\w-]+)/profile/$', views.profile_practitioner, name='practitioner_profile'),
-                       url(r'^physican/(?P<slug>[\w-]+)/review/$', views.review_practitioner, name='practitioner_review'),
-                       #Physican Registration related urls
-                       url(r'^physican/registration', views.registration_practitioner, name='practitioner_registration'),
-                       url(r'^physican/get-cond-proc-ajax/$', views.get_cond_proc_ajax, name='get_condition_procedure'),
-                       url(r'^physican/get-practice-names-ajax/$', views.get_practice_names_ajax, name='get_practice_names'),
-                       url(r'^physican/get-practice-details-ajax/$', views.get_practice_details_ajax, name='get_practice_details'),
-                       )
+  #Search Related urls
+  url(r'^physican/suggestions/$', views.suggestions_practitioner, name='physican_suggestions'),
+  url(r'^physican/search/$', views.search_practitioner, name='physican_search'),
+  url(r'^(?P<slug>[a-z-]+)-in-(?P<typee>[A-Za-z-]+)$', views.results_practitioner, name='physican_results'),
+  #Physican Page urls
+  url(r'^physican/(?P<slug>[\w-]+)/claim/$', views.claim_practitioner, name='claim_practitioner'),
+  url(r'^physican/(?P<slug>[\w-]+)/update/info/$', views.update_info_practitioner, name='update_info_practitioner'),
+  url(r'^physican/(?P<slug>[\w-]+)/profile/$', views.profile_practitioner, name='practitioner_profile'),
+  url(r'^physican/(?P<slug>[\w-]+)/review/$', views.review_practitioner, name='practitioner_review'),
+  #Physican Registration related urls
+  url(r'^physican/registration', views.registration_practitioner, name='practitioner_registration'),
+  url(r'^physican/get-cond-proc-ajax/$', views.get_cond_proc_ajax, name='get_condition_procedure'),
+  url(r'^physican/get-practice-names-ajax/$', views.get_practice_names_ajax, name='get_practice_names'),
+  url(r'^physican/get-practice-details-ajax/$', views.get_practice_details_ajax, name='get_practice_details'),
+  )
