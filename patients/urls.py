c45ab0d107604bc6a6a35252682f57e32db01430
Sarib Mahmood <saribmahmood55@gmail.com>
2015-08-18 18:16:25 +0500

    patient page changes

--- patients/urls.py
+++ patients/urls.py
@@ -2,11 +2,12 @@ from django.conf.urls import patterns, url
 from patients import views
 
 urlpatterns = patterns('',
-	url(r'^patient', views.patient, name='patient'),
-	url(r'^subscribe/$', views.subscribe, name='subscribe'),
-	url(r'^WhoIsMyDoctor', views.educate, name='WhoIsMyDoctor'),
+	url(r'^patient/physican', views.physican_patient, name='patient_physican'),
+	url(r'^patient/profile', views.profile_patient, name='patient_profile'),
+	url(r'^patient/subscribe/$', views.subscribe_patient, name='subscribe'),
+	url(r'^WhoIsMyDoctor', views.educate_patient, name='WhoIsMyDoctor'),
 	url(r'^favt_pract', views.favt_pract, name='add_favt_pract'),
-	url(r'^dashboard/profile/$', views.profile),
-	url(r'^dashboard/account/$', views.acc_preferences),
+	#url(r'^dashboard/profile/$', views.profile),
+	url(r'^patient/preferences/$', views.preferences_patient, name='patient_preferences'),
 	url(r'^dashboard/specialities/$', views.dashboard_specialities)
 )
--- patients/views.py
+++ patients/views.py
@@ -13,48 +13,54 @@ from django.contrib.auth import authenticate
 from django.contrib.auth.decorators import login_required
 import json
 
-def subscribe(request):
-	slug = None
-	if request.method == 'POST':
-		data = {}
-		slug = request.POST.get('slug', None)
-		email = request.POST.get('email', None)
-		cell_number = request.POST.get('phone', None)
-		data['msg'] = newSubscription(slug, email, cell_number)
-		if request.is_ajax():
-			return HttpResponse(json.dumps(data), content_type="application/json")
-	return HttpResponseRedirect(reverse('practitioner', kwargs={'slug': slug}))
-
-def educate(request):
-	data = {}
-	if request.method == 'GET':
-		return render_to_response('patients/educate.html', {'data': data}, context_instance=RequestContext(request))
 
 @login_required(login_url='/accounts/login/')
-def profile(request):
-	data = {}
-	user = request.user
-	if request.method == 'GET':
-		try:
-			data['patient'] = Patient.patient_objects.patient_details(user)
-		except Patient.DoesNotExist:
-			raise Http404
-
-	elif request.method == 'POST':
-		patientDetails = {}
-		patientDetails['user'] = user
-		patientDetails['fname'] = request.POST.get('firstname', None)
-		patientDetails['lname'] = request.POST.get('lastname', None)
-		patientDetails['number'] = request.POST.get('cell_number', None)
-		patientDetails['age'] = request.POST.get('age_group', None)
-		patientDetails['gender'] = request.POST.get('gender', None)
-		updatePatientDetails(patientDetails)
-		data['patient'] = Patient.patient_objects.patient_details(user)
-
-	return render_to_response('patients/profile.html', {'data': data}, context_instance=RequestContext(request))
+def physican_patient(request):
+    data = Patient.objects.get(email=request.user.email)
+    return render_to_response('patients/favt_pract.html', {'data': data}, context_instance=RequestContext(request))
 
 @login_required(login_url='/accounts/login/')
-def acc_preferences(request):
+def profile_patient(request):
+    data = {}
+    user = request.user
+    if request.method == 'GET':
+        try:
+            data['patient'] = Patient.patient_objects.patient_details(user)
+        except Patient.DoesNotExist:
+            raise Http404
+
+    elif request.method == 'POST':
+        patientDetails = {}
+        patientDetails['user'] = user
+        patientDetails['fname'] = request.POST.get('firstname', None)
+        patientDetails['lname'] = request.POST.get('lastname', None)
+        patientDetails['number'] = request.POST.get('cell_number', None)
+        patientDetails['age'] = request.POST.get('age_group', None)
+        patientDetails['gender'] = request.POST.get('gender', None)
+        updatePatientDetails(patientDetails)
+        data['patient'] = Patient.patient_objects.patient_details(user)
+
+    return render_to_response('patients/profile.html', {'data': data}, context_instance=RequestContext(request))
+
+def subscribe_patient(request):
+    slug = None
+    if request.method == 'POST':
+        data = {}
+        slug = request.POST.get('slug', None)
+        email = request.POST.get('email', None)
+        cell_number = request.POST.get('phone', None)
+        data['msg'] = newSubscription(slug, email, cell_number)
+        if request.is_ajax():
+            return HttpResponse(json.dumps(data), content_type="application/json")
+    return HttpResponseRedirect(reverse('practitioner', kwargs={'slug': slug}))
+
+def educate_patient(request):
+    data = {}
+    if request.method == 'GET':
+        return render_to_response('patients/educate.html', {'data': data}, context_instance=RequestContext(request))
+
+@login_required(login_url='/accounts/login/')
+def preferences_patient(request):
 	data = {}
 	user = request.user
 	if request.method == "POST":
@@ -111,55 +117,6 @@ def dashboard_specialities(request):
 
 	return render_to_response('patients/interested_specialities.html', {'data': data}, context_instance=RequestContext(request))
 
-@login_required(login_url='/accounts/login/')
-def patient(request):
-	data = {}
-	user = request.user
-	if request.method == 'GET':
-		try:
-			data['patient'] = Patient.patient_objects.patient_details(user)
-			data['reviews'] = Review.review_objects.patient_reviews(user)
-			data['excluded_specialties'] = excludedSpecialities(data['patient'])
-		except Patient.DoesNotExist:
-			raise Http404
-	elif request.method == 'POST':
-		if request.user.is_authenticated():
-			patientDetails, response, type_ = {}, {}, None
-			type_ = request.POST.get('type_', None)
-			if type_ == "updatePatient":
-				patientDetails['user'] = user
-				patientDetails['fname'] = request.POST.get('fname', None)
-				patientDetails['lname'] = request.POST.get('lname', None)
-				patientDetails['number'] = request.POST.get('number', None)
-				patientDetails['age'] = request.POST.get('age', None)
-				patientDetails['gender'] = request.POST.get('gender', None)
-				updatePatientDetails(patientDetails)
-				data['patient'] = Patient.patient_objects.patient_details(user)
-				print "user info updated."
-				data['reviews'] = Review.review_objects.patient_reviews(user)
-			elif type_ == "deleteReview":
-				review_id = request.POST.get('id', None)
-				response['status_'] = deleteReview(review_id)
-				return HttpResponse(json.dumps(response), content_type="application/json")
-			elif type_ == "deletePrac":
-				slug = request.POST.get('prac_slug')
-				patient = Patient.patient_objects.patient_details(user)
-				response['status_'] = deleteFavtPrac(patient, slug)
-				return HttpResponse(json.dumps(response), content_type="application/json")
-			elif type_ == "updatereview":
-				review_id = request.POST.get('review_id')
-				review_text = request.POST.get('review_text')
-				response['status_'] = updateReview(review_id, review_text)
-				return HttpResponse(json.dumps(response), content_type="application/json")
-			elif type_ == "favourite":
-				slug = request.POST.get('favt_slug', '')
-				user = request.user
-				response['status_'] = favourite(user, slug)
-				return HttpResponse(json.dumps(response), content_type="application/json")
-		else:
-			data['patient'] = None
-	return render_to_response('patients/profile.html', {'data': data}, context_instance=RequestContext(request))
-
 def favt_pract(request):
 	slug = request.GET.get('slug','')
 	resp = favourite(request.user,slug)
@@ -173,7 +130,7 @@ def set_patient(sender, **kwargs):
 
 	p = Patient()
 	p.user = user
-	
+
 	if facebook_extra_data:
 		facebook_data = facebook_extra_data[0].extra_data
 		if facebook_data['gender']:
