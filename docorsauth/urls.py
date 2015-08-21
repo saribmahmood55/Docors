241ae45adba1a3d8fc0848497197768d15126d17
Sarib Mahmood <saribmahmood55@gmail.com>
2015-08-22 00:02:57 +0500

    docorsauth urls pep8 modification

--- docorsauth/urls.py
+++ docorsauth/urls.py
@@ -3,8 +3,28 @@ from django.conf.urls import patterns, url, include
 from docorsauth import views
 
 urlpatterns = patterns('',
-    url(r'^accounts/register/$', views.register, name='registration_register'),
-    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': 'index'}),
-    url(r'^accounts/activate/(?P<token>.+)/$', views.activate, name='activate_account'),
-    url(r'^accounts/', include('django.contrib.auth.urls')),
-)
+                       url(
+                           r'^accounts/register/$',
+                           views.register,
+                           name='registration_register'
+                       ),
+                       url(
+                           r'^accounts/login/$',
+                           views.custom_login,
+                           name='auth_login'
+                       ),
+                       url(
+                           r'^accounts/logout/$',
+                           'django.contrib.auth.views.logout',
+                           {'next_page': 'index'}
+                       ),
+                       url(
+                           r'^accounts/activate/(?P<token>.+)/$',
+                           views.activate,
+                           name='activate_account'
+                       ),
+                       url(
+                           r'^accounts/',
+                           include('django.contrib.auth.urls')
+                       ),
+                       )
